8e9a3f443eff667688261bbf0343d7688bb16cd1
GhostWing17 <ghostwing17@gmail.com>
2022-12-13 16:47:29 +0900

    대시보드 샘플본

---
+++ app_home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def run_home_app() :
+    img_url = 'https://blog.kakaocdn.net/dn/MIwEC/btqZ3gi5r2C/m9QnC7TlkSc9rLL1NuMcEK/img.png'
+    st.image(img_url)
+
+    st.text('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다. 데이터 분석 및 고객 정보를 넣으면, 얼마정도의 차를 구매할지를 예측해줍니다.')
+    
+
+    
+    
